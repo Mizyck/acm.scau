9c9c6f06af40564168437a287cc97ceee4f6d089
Mizyck <68177186+Mizyck@users.noreply.github.com>
2020-12-09 16:15:45 +0800

    Create 1042.c

---
+++ 1042.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int s1 = 0, s2 = 0, num;
+	int day = 0, give = 1;
+	scanf("%d", &num);
+	num = num * 100;
+	while (s1 < s2)
+	{
+		s1 += give;
+		s2 += num;
+		give = give * 2;
+		day++;
+	}
+	printf("%d", day);
+	return 0;
+
+}
