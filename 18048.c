bb1e7f440a36fd24e0499560e02f4b408abd7e3a
Mizyck <68177186+Mizyck@users.noreply.github.com>
2021-01-07 18:42:59 +0800

    Create 18048.c

---
+++ 18048.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+float high(int n);
+float tan(int n);
+int main()
+{
+   float h = 0, t = 0;
+   int n, i;
+   scanf("%d", &n);
+   h = high(n);
+   t = tan(n);
+   printf("%.3f %.3f", h, t);
+   return 0;
+}
+float high(int n)
+{
+   int i;
+   float h = 100, s = 0;
+   for (i = 0; i < n; i++)
+   {
+      s += h;
+      h = h / 2;
+   }
+   return s;
+}
+float tan(int n)
+{
+   int i;
+   float h = 100;
+   for (i = 0; i < n; i++)
+   {
+      h = h / 2;
+   }
+   return h;
+}
