d72a0451712c138432b53a5e1c05d3cc3173a09f
Mizyck <68177186+Mizyck@users.noreply.github.com>
2021-01-07 19:18:47 +0800

    Update 18048.c

--- 18048.c
+++ 18048.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 float high(int);
-float tan(int);
+float tq(int);
 int main()
 {
    float h = 0, t = 0;
    int n, i;
    scanf("%d", &n);
    h = high(n);
-   t = tan(n);
+   t = tq(n);
    printf("%.3f %.3f", h, t);
    return 0;
 }
@@ -17,12 +17,12 @@ float high(int n)
    float h = 100, s = 0;
    for (i = 0; i < n; i++)
    {
-      s += h;
+      s += 2 * h;
       h = h / 2;
    }
-   return s;
+   return s - 100;
 }
-float tan(int n)
+float tq(int n)
 {
    int i;
    float h = 100;
