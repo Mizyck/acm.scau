cd5f3df5295a45b935cc55c5a89457083243a861
Mizyck <68177186+Mizyck@users.noreply.github.com>
2021-01-10 20:14:43 +0800

    Create 18073.c

---
+++ 18073.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+double cal(float, float, float);
+int main()
+{
+    float d, p, r;
+    double m;
+    scanf("%f %f %f", &d, &p, &r);
+    m = cal(d, p, r);
+    if (m == -1)
+    {
+        printf("God");
+        return 0;
+    }
+    
+    printf("%.1lf", m);
+    return 0;
+
+}
+double cal(float d, float p, float r)
+{
+    double m;
+    if (0 < p - d * r && 1 != 1 + r)
+    {
+        m = log(p / (p - d * r)) / log(1 + r);
+        return m;
+    }
+    else
+    {
+        return -1;
+    }
+}
